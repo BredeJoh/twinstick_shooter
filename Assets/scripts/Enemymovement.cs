4dad37e2226af733f2a222fdc9db2e906d64a910
emiven <emiven@hotmail.no>
2016-09-27 11:28:15 +0200

    enemymovement update

--- Assets/scripts/Enemymovement.cs
+++ Assets/scripts/Enemymovement.cs
@@ -2,29 +2,34 @@
 using System.Collections;
 
 public class Enemymovement : MonoBehaviour {
-
+	public GameObject spiller;
     public Transform enemylazerprefab;
     private int teller = 0;
-    Vector3 move;
-    public float speed = 1.0f;
+    public float fart = 5.0f;
 	// Use this for initialization
 	void Start () {
-        if (transform.position.y == -32)
-        {
-            speed = -1.0f;
-        }
+//        if (transform.position.y == -32)
+//        {
+//            speed = -1.0f;
+//        }
+	}
+	void moveTowards()
+	{
+		gameObject.transform.position = Vector2.MoveTowards (gameObject.transform.position, spiller.transform.position , Time.deltaTime * fart);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        move = new Vector3(0f, -0.1f, 0f);
-        transform.position += move * speed;
         teller++;
         if (teller == 120 && Health.playerHealth != 0)
         {
             Instantiate(enemylazerprefab, transform.position, transform.rotation);
             teller = 0;
         }
+	}
+	void FixedUpdate()
+	{
+		moveTowards();
 	}
     void OnTriggerExit2D(Collider2D other)
     {

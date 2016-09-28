0179115ea12d9df8f5db40374d38dc848022173e
André Engen <zornarka@gmail.com>
2016-09-28 14:48:16 +0200

    Added a game over menu and script

---
+++ Assets/scripts/GameOver.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+	string levelToLoad = "main";
+	string pauseButton = "pause";
+	string quitButton = "quit";
+	public GameObject GameOverScreen;
+
+	// Use this for initialization
+	void Start () {
+		GameOverScreen.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DeathCheck();
+	}
+
+	//stop the game and display a game over screen
+	void DeathCheck() {
+		if (Health.playerHealth <= 0) {
+			GameOverScreen.SetActive(true);
+			Time.timeScale = 0f;
+		}
+	}
+
+	void RetryLevel() {
+		if (GameOverScreen.activeSelf) {
+			if (Input.GetButtonDown(pauseButton)) {
+				SceneManager.LoadScene(levelToLoad, LoadSceneMode.Single);
+			}
+			if (Input.GetButtonDown(quitButton)) {
+				Application.Quit();
+			}
+		}
+
+	}
+}

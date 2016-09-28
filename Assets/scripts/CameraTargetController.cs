167166420979e920715efafb00917560e08f4543
oddbjornborg <oddbjorn.sb@gmail.com>
2016-09-28 10:27:36 +0200

    camera tweak

--- Assets/scripts/CameraTargetController.cs
+++ Assets/scripts/CameraTargetController.cs
@@ -24,14 +24,13 @@ public class CameraTargetController : MonoBehaviour {
 		mousePos = Camera.main.ScreenToWorldPoint (mousePos);
 
 		//Mouse Position compared to Player Avatar
-		Vector3 pPos = player.gameObject.transform.position;
-		mousePos.Set (mousePos.x - pPos.x, mousePos.y - pPos.y, -10);
+		Vector3 cameraPos = Camera.main.transform.position;
+		mousePos.Set (mousePos.x - cameraPos.x, mousePos.y - cameraPos.y, -10);
 
 		float angle = Mathf.Atan2 (mousePos.y, mousePos.x) * Mathf.Rad2Deg;
 		float dist = Mathf.Sqrt (Mathf.Pow (mousePos.x, 2) + Mathf.Pow (mousePos.y, 2));
 
-		//if (dist > maxCameraDistance)
-		//	dist = maxCameraDistance;
+		//dist = 1 + Mathf.Log10 (dist);
 
 		float distx = dist * Mathf.Cos (angle * Mathf.Deg2Rad);
 		float disty = dist * Mathf.Sin (angle * Mathf.Deg2Rad);
@@ -39,7 +38,7 @@ public class CameraTargetController : MonoBehaviour {
 		Vector3 temp = new Vector3(distx, disty, -10);
 
 		//Move Camera Target
-		transform.position = player.transform.position + temp / 2;
+		transform.position = player.transform.position + temp / 4;
 		Camera.main.transform.position = Vector3.Lerp (Camera.main.transform.position, transform.position, 1f);
 
 	}
